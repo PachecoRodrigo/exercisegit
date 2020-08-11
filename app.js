360ce0e9d900098c4d7401044fe838fa44868279
PachecoRodrigo <chrisianpachecoayza@gmail.com>
2020-08-11 17:18:32 -0300

    modificado console log

--- app.js
+++ app.js
@@ -1,6 +1,2 @@
-<<<<<<< HEAD
-console.log('Hola Mundo aca esta git');
-=======
-console.log('Hola Mundoo!');
->>>>>>> 8cd381f5d54357e01f7387aae617184cc9a303a4
+console.log('Hola Mundoo! este es el console log');
 tarea = require('./tareas');
